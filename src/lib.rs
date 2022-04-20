5499fe1157b312bec29ab3ca3546e45f54d98255
Adam Jermstad <acc@amjerm.me>
2022-04-19 22:07:22 -0500

    feat: type and prepend flags

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,56 @@
-pub fn get_branch_name(branch_type: String, a: &str, b: &str) -> String {
+pub struct Config {
+    branch_type: String,
+    prepended_value: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Self {
+        let mut branch_type = String::from("feature");
+        let mut prepended_value = String::from("");
+
+        for (i, argument) in args.iter().enumerate() {
+            match argument.as_str() {
+                "-t" | "--type" => {
+                    branch_type = args[i + 1].clone();
+                }
+                "-p" | "--prepend" => {
+                    prepended_value = args[i + 1].clone();
+                }
+                _ => (),
+            }
+        }
+
+        Self {
+            branch_type,
+            prepended_value,
+        }
+    }
+}
+
+pub fn run(config: Config, input: &str) -> String {
+    match input.trim().to_string().split_once("\t") {
+        Some((a, b)) => get_branch_name(config, a, b),
+        None => panic!("No delimiter found"),
+    }
+}
+
+pub fn get_branch_name(config: Config, a: &str, b: &str) -> String {
     let branch_name = format!(
         "{}/{}-{}",
-        get_prefix(branch_type),
+        get_prefix(config),
         a,
         b.to_lowercase().replace(" ", "-")
     );
     truncate_branch_name(&branch_name).to_string()
 }
 
-fn get_prefix(branch_type: String) -> String {
-    let mut name_segment = "".to_owned();
-    name_segment.push_str(branch_type.as_str());
-    name_segment
+fn get_prefix(config: Config) -> String {
+    let mut prefix = config.prepended_value;
+    if prefix.len() > 0 {
+        prefix.push('/');
+    }
+    prefix.push_str(config.branch_type.as_str());
+    prefix
 }
 
 fn truncate_branch_name(branch_name: &str) -> &str {
@@ -26,21 +65,47 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_get_branch_name() {
-        let expected = "adam/feature/APM-123-do-something-that-h";
+    fn test_run() {
+        let args: Vec<String> = vec![
+            String::from("-p"),
+            String::from("fx"),
+            String::from("-t"),
+            String::from("zammo"),
+        ];
+        let config = Config::new(&args);
+        let expected = "fx/zammo/APM-123-do-something-that-helps";
         assert_eq!(
-            get_branch_name(
-                String::from("feature"),
-                "APM-123",
-                "Do something that helps"
-            ),
+            run(config, "APM-123\tDo something that helps the product"),
+            expected
+        );
+    }
+
+    #[test]
+    fn test_get_branch_name() {
+        let args: Vec<String> = vec![
+            String::from("-p"),
+            String::from("fx"),
+            String::from("-t"),
+            String::from("zammo"),
+        ];
+        let config = Config::new(&args);
+        let expected = "fx/zammo/APM-123-do-something-that-helps";
+        assert_eq!(
+            get_branch_name(config, "APM-123", "Do something that helps the product"),
             expected
         );
     }
 
     #[test]
     fn test_get_prefix() {
-        assert_eq!(get_prefix(String::from("feature")), "adam/feature");
+        let args: Vec<String> = vec![
+            String::from("-p"),
+            String::from("fx"),
+            String::from("-t"),
+            String::from("zammo"),
+        ];
+        let config = Config::new(&args);
+        assert_eq!(get_prefix(config), "fx/zammo");
     }
 
     #[test]
--- src/main.rs
+++ src/main.rs
@@ -2,51 +2,17 @@ use branchify;
 use std::env;
 use std::io;
 
+use branchify::{run, Config};
+
 fn main() {
+    let args: Vec<String> = env::args().skip(1).collect();
+    let config = Config::new(&args);
+
     let mut input = String::new();
     match io::stdin().read_line(&mut input) {
         Ok(_) => {
-            print!("{}", process_input(&input))
+            print!("{}", run(config, &input))
         }
         Err(_) => print!("{}", ""),
     }
-}
-
-fn process_input(input: &str) -> String {
-    let branch_type = parse_options().branch_type;
-    match input.trim().to_string().split_once("\t") {
-        Some((a, b)) => branchify::get_branch_name(branch_type, a, b),
-        None => panic!("No delimiter found"),
-    }
-}
-
-struct Options {
-    branch_type: String,
-}
-
-impl Options {
-    fn new(branch_type: String) -> Self {
-        Self {
-            branch_type: branch_type,
-        }
-    }
-}
-
-fn parse_options() -> Options {
-    let mut args = env::args();
-    let branch_type: String;
-
-    // ignoring the first argument
-    args.next();
-    match args.next() {
-        Some(arg) => branch_type = arg,
-        None => branch_type = "feature".to_string(),
-    }
-    Options::new(branch_type)
-}
-
-#[test]
-fn test_process_input() {
-    let expected = "adam/feature/APM-123-do-something-that-h";
-    assert_eq!(process_input("APM-123\tDo something that helps"), expected);
 }
